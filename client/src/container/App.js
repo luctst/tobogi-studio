bc16293202011f6f55bfcdfd9fdd5132fd7f57d7
Lucas Tostée <lucas.tostee@gmail.com>
2019-11-09 00:30:49 +0100

    Add main component with routes and redux

--- client/src/container/App.js
+++ client/src/container/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import Container from "./../styles/Container";
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import Header from './../components/Header/Header';
+import {Provider} from "react-redux";
+import store from "./../state/store";
 
 function App() {
   return (
-    <div>
-    </div>
+    <Container>
+      <BrowserRouter forceRefresh={false}>
+        <Provider store={store}>
+          <Header/>
+          <Switch>
+            <Route exact strict path="/" render={() => <h1>Hello world</h1>}/>
+          </Switch>
+        </Provider>
+      </BrowserRouter>
+    </Container>
   );
 }
 
