ac72c7ae07e737928d8297b710e6c703ef8a9e1c
Lucas Tostée <lucas.tostee@gmail.com>
2019-11-10 23:16:43 +0100

    Add sidabr handler component

---
+++ client/src/container/SidebarHandler/Sidebar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Sidebar from "./../../components/Sidebar/Sidebar";
+import {connect} from 'react-redux';
+
+function SidebarHandler (props) {
+    const mainRoot = document.querySelector("#root");
+
+    if (props.showSidebar) {
+        const createModal = document.createElement("section");
+
+        if (window.innerWidth >= 560) mainRoot.style = "display: grid;grid-template-columns: 2.5fr 1fr;";
+
+        createModal.id = "modal";
+        mainRoot.appendChild(createModal);
+
+        return ReactDOM.createPortal(
+            <Sidebar/>,
+            createModal
+        )
+    }
+
+    if (mainRoot.querySelector("#modal")) {
+        if (mainRoot.style.length !== 0) mainRoot.removeAttribute("style");
+        mainRoot.removeChild(document.querySelector("#modal"))
+    };
+
+    return null;
+}
+
+function mapState ({sidebarReducer}) {
+    return sidebarReducer;
+}
+
+export default connect(mapState)(SidebarHandler);
