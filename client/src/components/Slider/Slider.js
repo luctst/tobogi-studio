e331b97ff296a279e9dc7de9cd260110b7214c84
Lucas Tostée <lucas.tostee@gmail.com>
2019-11-26 23:23:57 +0100

    Create slider component

---
+++ client/src/components/Slider/Slider.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Slider (props) {
+    console.log(props);
+    return (
+        <section className="swiper-container">
+            <div className="swiper-wrapper">
+
+            </div>
+        </section>
+    );
+}
+
+export default Slider;
