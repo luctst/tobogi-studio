f0861d48413d3ddc538964b63d954fb5650e1302
Lucas Tostée <lucas.tostee@gmail.com>
2019-11-27 03:13:24 +0100

    Remove useless style

--- client/src/components/Project/Project.style.js
+++ client/src/components/Project/Project.style.js
@@ -4,28 +4,33 @@ import Container from "./../../styles/Container";
 const ProjectsHandlerSC = styled(Container)`
     display: flex;
     flex-direction: column;
-    height: 90vh;
-    max-height: 90vh;
+    height: auto;
 
     div {
+        height: fit-content;
         margin: auto;
         text-align: center;
 
         img {
             object-fit: cover;
             height: auto;
-            width: 70%;
+            width: 100%;
         }
     }
 
     footer {
+        text-align: center;
+
         p {
+            font-size: 15px;
+            line-height: 1.6;
             text-transform: uppercase;
         }
 
         button {
             background-color: transparent;
             border: none;
+            font-size: 15px;
             font-weight: 500;
             text-decoration: underline;
             text-transform: uppercase;
@@ -33,40 +38,6 @@ const ProjectsHandlerSC = styled(Container)`
 
             &:hover {
                 cursor: pointer;
-            }
-        }
-    }
-
-    @media screen and (min-width: 570px) {
-        footer {
-            display: flex;
-
-            p,
-            button {
-                font-size: 14px;
-            }
-        }
-    }
-
-    @media screen and (max-width: 570px) {
-        display: block;
-
-        div {
-            img {
-                width: 100%;
-            }
-        }
-
-        footer {
-            text-align: center;
-
-            p {
-                font-size: 15px;
-                line-height: 1.6;
-            }
-
-            button {
-                font-size: 15px;
             }
         }
     }
