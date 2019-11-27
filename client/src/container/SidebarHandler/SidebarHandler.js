f151165763d1ce3e4375105e2bd19e77fb03db3b
Lucas Tostée <lucas.tostee@gmail.com>
2019-11-27 03:16:38 +0100

    Rename Sidebar for SidebarHandler

--- client/src/container/SidebarHandler/SidebarHandler.js
+++ client/src/container/SidebarHandler/SidebarHandler.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Sidebar from "./../../components/Sidebar/Sidebar";
+import Sidebar from "../../components/Sidebar/Sidebar";
 import {connect} from 'react-redux';
 
 function SidebarHandler (props) {
