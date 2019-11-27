746292a4c00121cfab6b02a8398a6a78400078e3
Lucas Tostée <lucas.tostee@gmail.com>
2019-11-27 03:19:59 +0100

    Add windowWidthReducer to redux store

--- client/src/state/store.js
+++ client/src/state/store.js
@@ -1,8 +1,10 @@
 import {createStore, combineReducers} from 'redux';
 import sidebarReducer from "./reducers/sidebarReducer";
+import windowWidthReducer from "./reducers/windowWidthReducer";
 
 const reducersRoot = combineReducers({
     sidebarReducer,
+    windowWidthReducer
 });
 
 const store = createStore(reducersRoot);
