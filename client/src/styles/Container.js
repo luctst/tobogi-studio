42f3caf7406657a2512b5ae10f4145dc3415b5aa
Lucas Tostée <lucas.tostee@gmail.com>
2019-11-29 01:07:47 +0100

    Add max-width for media queries

--- client/src/styles/Container.js
+++ client/src/styles/Container.js
@@ -13,17 +13,21 @@ const Container = styled.section`
 
 	@media screen and (min-width: 577px) {
 		width: 540px;
+		max-width: 540px;
 	}
 
 	@media screen and (min-width: 768px) {
 		width: 720px;
+		max-width: 720px;
 	}
 
 	@media screen and (min-width: 992px) {
 		width: 960px;
+		max-width: 960px;
 	}
 
 	@media screen and (min-width: 1200px) {
+		width: 1140px;
 		max-width: 1140px;
 	}
 `;
